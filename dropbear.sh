abd9a19cac5b60bdf6155f414f733b5e70874be5
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-06-07 20:45:43 +0700

    Update dropbear.sh

--- dropbear.sh
+++ dropbear.sh
@@ -1,7 +1,7 @@
 apt-get install -y dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=222/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 "/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS=""/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 service ssh restart
