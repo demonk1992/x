f85edf508685371cee54439bd0cad943b8d0d829
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-06-07 20:48:35 +0700

    Update stunnel.sh

--- stunnel.sh
+++ stunnel.sh
@@ -11,6 +11,7 @@ connect = 127.0.0.1:443
 accept = 3128
 EOF
 sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+# Generate Certificate
 openssl req -new -newkey rsa:2048 -days 3650 \
   -nodes -x509 -sha256 \
   -subj '/CN=LocalHost' \
