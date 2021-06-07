56a895b45840bb3267831f4a1b31bf578539c768
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-06-07 19:06:36 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+# update upgrade
+export DEBIAN_FRONTEND=noninteractive
+apt-get update; apt-get -y dist-upgrade;
+apt-get install -y ca-certificates apt-transport-https lsb-release screen unzip wget curl git netcat tcpd tor build-essential checkinstall zlib1g-dev libsctp-dev cmake make gcc zlib1g-dev libwrap0-dev certbot tor perl libperl-dev libgd3 libgd-dev libgeoip1 libgeoip-dev geoip-bin libxml2 libxml2-dev libxslt1.1 libxslt1-dev build-essential git tree libpcre3-dev socket libssl-dev libssl1.0 dh-autoreconf libssh-4 libssh-dev libconfig9 lolcat mercurial
+# Create and Configure rc.local
+cat > /etc/rc.local <<-END
+#!/bin/sh -e
+exit 0
+END
+chmod +x /etc/rc.local
+# Python Proxy
+wget https://github.com/demonk1992/vps/raw/main/proxy.py
+# Create rc.d
+mkdir /etc/rc.d
+chmod +x /etc/rc.d
+cat > /etc/rc.d/rc.local <<-END
+#!/bin/sh -e
+exit 0
+END
+chmod +x /etc/rc.d/rc.local
+# set time GMT +7
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
+#  set locale
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+# dsable ipv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
+# Upgrade openssl
+wget https://raw.githubusercontent.com/demonk1992/x/main/openssl.sh && chmod +x openssl.sh && screen -S openssl ./openssl.sh
+# sslh
+wget https://raw.githubusercontent.com/demonk1992/x/main/sslh.sh && chmod +x sslh.sh && screen -S sslh ./sslh.sh
+# Install badvpn deb/ubun
+wget https://raw.githubusercontent.com/demonk1992/x/main/badvpn.sh && chmod +x badvpn.sh && screen -S badvpn ./badvpn.sh
+# Dropbear
+wget https://raw.githubusercontent.com/demonk1992/x/main/dropbear.sh && chmod +x dropbear.sh && screen -S dropbear ./dropbear.sh
+# set ipv4 forward
+echo 1 > /proc/sys/net/ipv4/ip_forward
+sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+# Stunel
+wget https://raw.githubusercontent.com/demonk1992/x/main/stunnel.sh && chmod +x stunnel.sh && screen -S stunnel ./stunnel.sh
+# Boost TCP
+wget https://raw.githubusercontent.com/demonk1992/x/main/tcp.sh && chmod +x tcp.sh && screen -S tcp ./tcp.sh
+# v2ray
+wget https://multi.netlify.app/v2ray.sh && chmod +x v2ray.sh && screen -S v2ray ./v2ray.sh
+# Setup l2tp
+wget https://github.com/demonk1992/vps/raw/main/vpn-install.tar.gz && tar -xf vpn-install.tar.gz && chmod +x vpn-install/* && screen -S sudo bash vpn-install/ipsec/install.sh
+# Openvpn
+wget https://github.com/demonk1992/vps/raw/main/openvpn-install.sh && chmod +x openvpn-install.sh && screen -S openvpn-install ./openvpn-install.sh
+# SSHplus
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Plus && chmod +x Plus && screen -S Plus ./Plus
+history -c
+echo "1.2" > /home/ver
+clear
+echo " "
+echo "Installation has been completed!!"
+echo " "
+echo "=================================-Autoscript-===========================" | tee -a log-install.txt
+echo "" | tee -a log-install.txt
+echo "--------------------------------------------------------------------------------" | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22"  | tee -a log-install.txt
+echo "   - OpenVPN                 : UDP 1194"  | tee -a log-install.txt
+echo "   - Stunnel4                   : 3128"  | tee -a log-install.txt
+echo "   - Ssl                              : 443"  | tee -a log-install.txt
+echo "   - Dropbear                  : 222"  | tee -a log-install.txt
+echo "   - Badvpn                     : 7100, 7200, 7300, 7400, 7500, 7600, 7700, 7800, 7900"  | tee -a log-install.txt
+echo "   - Nginx                        : 8443"  | tee -a log-install.txt
+echo "   - L2TP/IPSEC VPN          : 1701"  | tee -a log-install.txt
+echo "   - V2RAY Vless None TLS    : 8880"  | tee -a log-install.txt
+echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
+echo "   - Timezone                : Asia/Jakarta (GMT +7)"  | tee -a log-install.txt
+echo "   - Fail2Ban                : [ON]"  | tee -a log-install.txt
+echo "   - Dflate                  : [ON]"  | tee -a log-install.txt
+echo "   - IPtables                : [ON]"  | tee -a log-install.txt
+echo "   - Auto-Reboot             : [ON]"  | tee -a log-install.txt
+echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
+echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
+echo ""  | tee -a log-install.txt
+echo "   - User Name               : Demonk"  | tee -a log-install.txt
+echo "   - Telegram                : T.me/sunatmasal"  | tee -a log-install.txt
+echo "   - Whatsapp                : 082119813096"  | tee -a log-install.txt
+echo "=================================-Autoscript-===========================" | tee -a log-install.txt
+echo ""
+rm -f x.sh
