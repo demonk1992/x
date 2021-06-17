c94c151e00d23ff6a69242a2385f76ce0ad176f7
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-06-18 06:59:14 +0700

    Add files via upload

---
+++ kill.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /usr/bin
+wget https://raw.githubusercontent.com/demonk1992/x/main/autokill.sh && chmod +x autokill.sh
+wget https://raw.githubusercontent.com/demonk1992/x/main/userlimit.sh && chmod +x chmod +x userlimit.sh
+wget https://raw.githubusercontent.com/demonk1992/x/main/userlimitssh.sh && chmod +x userlimitssh.sh
+wget https://raw.githubusercontent.com/demonk1992/x/main/clearcache.sh && chmod +x clearcache.sh
+cd /etc
+wget https://raw.githubusercontent.com/demonk1992/x/main/crontab && chmod +x crontab
+cd
+echo "@reboot root userlimit.sh" > /etc/cron.d/userlimit1
+echo "@reboot root userlimitssh.sh" > /etc/cron.d/userlimitssh1
+echo "@reboot root clearcache.sh" > /etc/cron.d/clearcache1
+sed -i '$ i\screen -dmS autokill ./autokill.sh' /etc/rc.local
