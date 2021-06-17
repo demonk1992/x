dd7fd98358356ce5c22465f3b7d7acac778c355b
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-06-18 05:09:26 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -1,15 +1,15 @@
 while :
   do 
-  ./userlimitssh.sh 1
+  userlimitssh.sh 1
   sleep 10
   done
 while :
   do
-  ./userlimit.sh 1
+  userlimit.sh 1
   sleep 10 
   done
 while :
   do
-  ./userexpired.sh
+  userexpired.sh
   sleep 36000
   done
