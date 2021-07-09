a1f5fa4e6b9ab9d269f73687bd44b396ceb1b410
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-07-10 01:47:58 +0700

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -2,3 +2,5 @@
 wget http://prdownloads.sourceforge.net/webadmin/webmin_1.979_all.deb
 dpkg -i webmin_1.979_all.deb
 apt-get install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python unzip
+sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
+service webmin restart
