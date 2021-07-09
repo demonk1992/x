a80af9655606b53098bb0ec3a2af269849004d82
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-07-10 00:29:35 +0700

    Add files via upload

---
+++ screenfetch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git
+echo "clear" >> .profile
+echo "neofetch" >> .profile
+echo "𝐈𝐃𝐒𝐒𝐇.𝐗𝐘𝐙" >> .profile
