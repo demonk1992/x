9a7b94b0a987dc3ee3c756ffa34582151ccd98a7
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-07-27 01:50:59 +0700

    Update sslh.sh

--- sslh.sh
+++ sslh.sh
@@ -7,6 +7,6 @@ RUN=yes
 
 DAEMON=/usr/sbin/sslh
 
-DAEMON_OPTS='--user sslh --listen 0.0.0.0:443 --ssh 127.0.0.1:222 --ssl 127.0.0.1:3128 --http 127.0.0.1:80 --openvpn 127.0.0.1:1194 --pidfile /var/run/sslh/sslh.pid'
+DAEMON_OPTS='--user sslh --listen 0.0.0.0:443 --ssh 127.0.0.1:222 --ssl 127.0.0.1:3127 --http 127.0.0.1:80 --openvpn 127.0.0.1:1194 --pidfile /var/run/sslh/sslh.pid'
 EOF
 sudo /etc/init.d/sslh restart
