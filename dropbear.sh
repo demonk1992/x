fe696aab5ee50d28951dc689275000a7681d0f10
demonk1992 <74846596+demonk1992@users.noreply.github.com>
2021-08-01 07:49:10 +0700

    Update dropbear.sh

--- dropbear.sh
+++ dropbear.sh
@@ -17,6 +17,32 @@ mv /usr/sbin/dropbear /usr/sbin/dropbear.old
 ln /usr/local/sbin/dropbear /usr/sbin/dropbear
 cd && rm -rf dropbear-2016.74 && rm -rf dropbear-2016.74.tar.bz2
 DROPBEAR=/etc/defaualt/dropbear
-sed -i "s/DROPBEAR_EXTRA_ARGS=""/DROPBEAR_EXTRA_ARGS=""/g" "$DROPBEAR"
-sed -i "s/DROPBEAR_EXTRA_ARGS=""/DROPBEAR_EXTRA_ARGS="/etc/idssh.net"/g" "$DROPBEAR"
+DROPBEAR=/etc/defaualt/dropbear
+cat > /etc/default/dropbear << "EOF"
+# disabled because OpenSSH is installed
+# change to NO_START=0 to enable Dropbear
+NO_START=0
+# the TCP port that Dropbear listens on
+DROPBEAR_PORT=222
+
+# any additional arguments for Dropbear
+DROPBEAR_EXTRA_ARGS=""
+
+# specify an optional banner file containing a message to be
+# sent to clients before they connect, such as "/etc/issue.net"
+DROPBEAR_BANNER="/etc/idssh.net"
+
+# RSA hostkey file (default: /etc/dropbear/dropbear_rsa_host_key)
+#DROPBEAR_RSAKEY="/etc/dropbear/dropbear_rsa_host_key"
+
+# DSS hostkey file (default: /etc/dropbear/dropbear_dss_host_key)
+#DROPBEAR_DSSKEY="/etc/dropbear/dropbear_dss_host_key"
+
+# ECDSA hostkey file (default: /etc/dropbear/dropbear_ecdsa_host_key)
+#DROPBEAR_ECDSAKEY="/etc/dropbear/dropbear_ecdsa_host_key"
+
+# Receive window size - this is a tradeoff between memory and
+# network performance
+DROPBEAR_RECEIVE_WINDOW=65536
+EOF
 service dropbear restart
